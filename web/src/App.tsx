bf57631940164bffac95cd37671034ec1da18c08
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-09-21 20:00:20 -0300

    feat(web) - test freetogame api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,34 @@
-import { MagnifyingGlassPlus } from 'phosphor-react'
+import { MagnifyingGlassPlus } from 'phosphor-react';
+import axios from 'axios';
 
 import './styles/main.css';
 import logoImg from './assets/logo_esports.svg';
+import { useEffect, useState } from 'react';
 
+interface GameProps {
+  id: string;
+  title: string;
+  genre: string;
+  game_url: string;
+  thumbnail: string;
+}
 
 function App() {
+  const [games, setGames] = useState<GameProps[]>();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const response = async () =>
+      axios
+        .get<GameProps[]>('http://localhost:3333/games')
+        .then(({ data }) => setGames(data))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+
+    response();
+  }, []);
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="logo" />
@@ -18,54 +42,28 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game2.png" alt="" />
+        {loading ? (
+          <>loading...</>
+        ) : (
+          games?.map((game) => (
+            <a
+              href={game.game_url}
+              className="relative rounded-lg overflow-hidden"
+              key={game.id}
+            >
+              <img src={game.thumbnail} alt="" />
 
-          <div className="w-full pt-16 pb-4 px-4 bg-text-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game name</strong>
-            <span className="text-zinc-300 text-sm block">Ads info</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game3.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-text-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game name</strong>
-            <span className="text-zinc-300 text-sm block">Ads info</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game4.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-text-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game name</strong>
-            <span className="text-zinc-300 text-sm block">Ads info</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game5.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-text-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game name</strong>
-            <span className="text-zinc-300 text-sm block">Ads info</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game6.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-text-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game name</strong>
-            <span className="text-zinc-300 text-sm block">Ads info</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game1.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-text-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game name</strong>
-            <span className="text-zinc-300 text-sm block">Ads info</span>
-          </div>
-        </a>
+              <div className="w-full pt-16 pb-4 px-4 bg-text-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">
+                  {game.title}
+                </strong>
+                <span className="text-zinc-300 text-sm block">
+                  {game.genre}
+                </span>
+              </div>
+            </a>
+          ))
+        )}
       </div>
 
       <div className="pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden">
@@ -78,7 +76,7 @@ function App() {
           </div>
 
           <button className="py-3 px-4 bg-violet-500 hover:via-violet-600 text-white rounded-lg flex gap-2 items-center">
-            <MagnifyingGlassPlus size={24}/>
+            <MagnifyingGlassPlus size={24} />
             Publish ads
           </button>
         </div>
